c8585d4b403462beff3886a7a4122991464eca66
arventwei <arventwei@163.com>
2014-03-10 00:15:48 +0800

    add log

---
+++ app/log.py
@@ -0,0 +1,37 @@
+import logging
+
+
+def initLog():
+    print "init Log"
+    logger = logging.getLogger('mylogger')
+    logger.setLevel(logging.DEBUG)
+
+
+    fh = logging.FileHandler('test.log')
+    fh.setLevel(logging.DEBUG)
+    
+    
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    
+    
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+def info(content):
+    logger = logging.getLogger('mylogger')
+    logger.info(content)
+def debug(content):
+    logger = logging.getLogger('mylogger')
+    logger.debug(content)
+def error(content):
+    logger = logging.getLogger('mylogger')
+    logger.error(content)
+def log(content):
+    logger = logging.getLogger('mylogger')
+    logger.log(content)
--- app/xiaoxin.py
+++ app/xiaoxin.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 from flask import request
 from app import app
-
-
+from log import info
+#test data
+#curl --data "birthyear=1905&press=%20OK%20" http://127.0.0.1:9999/xiaoxin/config
+#
 @app.route('/xiaoxin/<action>', methods=['GET', 'POST'])
 def xiaoxin(action):
     #return "Failed"
-    ret="==-=-=-=-=-=-=-="
+    
+    ret="Ok"
+    if "sn" not in request.form:
+        return "Fail sn is null"
+    sn = request.form["sn"]
+    userid =request.form["userid"]
+   
+        
+    if userid == None:
+        return "Fail userid is null";
+    info(request.form)
    # print request.json
     if request != None:
         ret+= str(request.data).lower()
