7fbf68138d8a8aadb98067c87514cff3de30a4fb
arventwei <arventwei@163.com>
2014-03-09 22:21:25 +0800

    add pydev

--- app/xiaoxin.py
+++ app/xiaoxin.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
+from flask import request
 from app import app
 
 
-@app.route('/xiaoxin')
-def xiaoxin():
-    return "Hello, World!"
+@app.route('/xiaoxin/<action>', methods=['GET', 'POST'])
+def xiaoxin(action):
+    #return "Failed"
+    print request.json
+    if request != None:
+        print request.json
+    print "=========================="
+    #if action =="config":
+    #    return xiaoxin_config(request.json)
+    return "ok"
+    #return "Hello, World !"+action
+
+def xiaoxin_config(content):
+    return content;
+    #pass
